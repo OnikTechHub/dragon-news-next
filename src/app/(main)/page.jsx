8a560033c8c94b17c733bb4df7259a7c4e2f4019
onik-999 <onikdas.dev@gmail.com>
2026-04-28 01:02:50 +0600

    Load News Data & Filter News Based On Category

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,29 +1,11 @@
-import LeftSidebar from "@/components/homepage/news/LeftSidebar";
-import RightSidebar from "@/components/homepage/news/RightSidebar";
-import Image from "next/image";
+import { redirect } from "next/navigation"; 
 
-async function getCategories() {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/news/categories",
-  );
-  const data = await res.json();
-  return data.data;
-}
-export default async function Home() {
-  const categories = await getCategories();
-  console.log(categories.news_category, "categories");
+const default_catagory_id = "01";
 
-  return (
-    <div className="w-11/12 mx-auto grid grid-cols-12 gap-4 my-[60px]">
-      <div className="font-bold text-lg col-span-3">
-        <LeftSidebar categories={categories} activeId={"null"}></LeftSidebar>
-      </div>
-      <div className="font-bold text-3xl bg-purple-100 col-span-6">
-        Dragon News Home
-      </div>
-      <div className=" col-span-3">
-        <RightSidebar></RightSidebar>
-      </div>
-    </div>
-  );
-}
+const Home = async () => {
+  
+  redirect(`/category/${default_catagory_id}`);
+
+};
+
+export default Home;
--- src/components/homepage/news/LeftSidebar.jsx
+++ src/components/homepage/news/LeftSidebar.jsx
@@ -10,7 +10,7 @@ const LeftSidebar = ({ categories, activeId }) => {
                     return (
                         <li
                             key={category.category_id}
-                            className={`${activeId === category.category_id && "bg-slate-100"} p-2 rounsed-md font-bold text-center text-md`}
+                            className={`${activeId === category.category_id && "bg-purple-500 text-white "}  rounded-md font-bold text-center text-md`}
                         >
                             <Link href={`/category/${category.category_id}`} className='block  p-2'>
                             {" "}
