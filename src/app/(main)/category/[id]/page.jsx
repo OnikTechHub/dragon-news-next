f3c55d3f18a259347d9e4e853be005cd02b88d5c
onik-999 <onikdas.dev@gmail.com>
2026-04-28 12:52:01 +0600

    Display News Card And Handle Loading

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,4 +1,5 @@
 import LeftSidebar from '@/components/homepage/news/LeftSidebar';
+import NewsCard from '@/components/homepage/news/NewsCard';
 import RightSidebar from '@/components/homepage/news/RightSidebar';
 import { getCategories, getNewsByCategoryId } from '@/lib/data';
 import React from 'react';
@@ -17,15 +18,15 @@ const NewsCategoryPage = async ({ params }) => {
             <div className="font-bold text-lg col-span-3">
                 <LeftSidebar categories={categories} activeId={id}></LeftSidebar>
             </div>
-            <div className="font-bold col-span-6">
+            <div className=" col-span-6">
                 <h2 className='font-bold text-lg'>Dragon News Home</h2>
                 <div className="space-y-4 mt-6">
                     {news.length > 0 ? news.map((n) => {
                         return (
-                            <div key={n._id} className="p-6 rounded-md border">{n.title}
-                        </div>
+                            <NewsCard key={n._id} news={n}>{n.title}
+                            </NewsCard>
                         );
-                        
+
                     }) : <h2 className='font-bold text-4xl text-center my-7'>No news found </h2>}
                 </div>
             </div>
