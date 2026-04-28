2b5c83c8b8f8d25f19171fe18f0441e6df5f3200
onik-999 <onikdas.dev@gmail.com>
2026-04-28 22:31:07 +0600

    Register User With Email & Password And Signin

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,32 +1,40 @@
 "use client"
+import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
-    const { register,handleSubmit, formState: {errors}, } = useForm()
+    const { register, handleSubmit, formState: { errors }, } = useForm()
 
-    const handleLoginFunc = (data) => {
-       console.log(data, "data")
+    const handleLoginFunc = async (data) => {
+        console.log(data, "data")
 
+        const { data: res, error } = await authClient.signIn.email({
+            email: data.email, // required
+            password: data.password, // required
+            rememberMe: true,
+            callbackURL: "/",
+        });
+        console.log(res, errors)
     }
-    console.log(errors, "errors")
+    
 
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
             <div className='p-8 rounded-xl bg-white shadow-lg w-full max-w-md'>
                 <h2 className='font-bold text-3xl text-center mb-6'>Login your account</h2>
 
-                
+
                 <form className="space-y-4" onSubmit={handleSubmit(handleLoginFunc)}>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Email</legend>
-                        <input 
-                            type="email" 
-                            className="input w-full border p-2 rounded" 
-                           
-                            placeholder="Type here email" 
-                             
+                        <input
+                            type="email"
+                            className="input w-full border p-2 rounded"
+
+                            placeholder="Type here email"
+
                             {...register("email", { required: "Email field is required" })}
                         />
 
@@ -35,11 +43,11 @@ const LoginPage = () => {
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Password</legend>
-                        <input 
-                            type="password" 
-                            className="input w-full border p-2 rounded"  
-                            placeholder="Type here password" 
-                            
+                        <input
+                            type="password"
+                            className="input w-full border p-2 rounded"
+                            placeholder="Type here password"
+
 
                             {...register("password", { required: "Password field is required" })}
                         />
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,35 +1,51 @@
 "use client"
+import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const RegisterPage = () => {
-    const { register,handleSubmit, formState: {errors}, } = useForm()
+    const { register, handleSubmit, formState: { errors }, } = useForm()
 
-    const handleRegisterFunc = (data) => {
-       console.log(data, "data")
+    const handleRegisterFunc = async (data) => {
+        console.log(data, "data")
 
-       const { email, name, photo, password} = data;
-       console.log(name, photo)
+        const { email, name, photo, password } = data;
+        console.log(name, photo)
 
+        const { data: res, error } = await authClient.signUp.email({
+            name: name, // required
+            email: email, // required
+            password: password, // required
+            image: photo,
+            callbackURL: "/",
+        })
+         console.log(res, error)
+         if(error){
+            alert(error.message)
+         }
+
+         if(res){
+            alert("Signup seccessfull")
+         }
     }
-    console.log(errors, "errors")
+   
 
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
             <div className='p-8 rounded-xl bg-white shadow-lg w-full max-w-md'>
                 <h2 className='font-bold text-3xl text-center mb-6'>Register your account</h2>
 
-                
+
                 <form className="space-y-4" onSubmit={handleSubmit(handleRegisterFunc)}>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Name</legend>
-                        <input 
-                            type="Name" 
-                            className="input w-full border p-2 rounded" 
-                           
-                            placeholder="Type here name" 
-                             
+                        <input
+                            type="Name"
+                            className="input w-full border p-2 rounded"
+
+                            placeholder="Type here name"
+
                             {...register("name", { required: "Name field is required" })}
                         />
 
@@ -37,12 +53,12 @@ const RegisterPage = () => {
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Photo URL</legend>
-                        <input 
-                            type="text" 
-                            className="input w-full border p-2 rounded" 
-                           
-                            placeholder="Type here photo url" 
-                             
+                        <input
+                            type="text"
+                            className="input w-full border p-2 rounded"
+
+                            placeholder="Type here photo url"
+
                             {...register("photo", { required: "Photo URL field is required" })}
                         />
 
@@ -50,12 +66,12 @@ const RegisterPage = () => {
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Email</legend>
-                        <input 
-                            type="email" 
-                            className="input w-full border p-2 rounded" 
-                           
-                            placeholder="Type here email" 
-                             
+                        <input
+                            type="email"
+                            className="input w-full border p-2 rounded"
+
+                            placeholder="Type here email"
+
                             {...register("email", { required: "Email field is required" })}
                         />
 
@@ -64,11 +80,11 @@ const RegisterPage = () => {
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Password</legend>
-                        <input 
-                            type="password" 
-                            className="input w-full border p-2 rounded"  
-                            placeholder="Type here password" 
-                            
+                        <input
+                            type="password"
+                            className="input w-full border p-2 rounded"
+                            placeholder="Type here password"
+
 
                             {...register("password", { required: "Password field is required" })}
                         />
